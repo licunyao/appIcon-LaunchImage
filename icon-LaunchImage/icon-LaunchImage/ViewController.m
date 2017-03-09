d781b195a25671ef671224b4036cafbb7fd70e0b
licunyao <1436120982@qq.com>
2017-03-09 17:09:22 +0800

    初始化创建工程

---
+++ icon-LaunchImage/icon-LaunchImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  icon-LaunchImage
+//
+//  Created by 李存瑶 on 17/3/9.
+//  Copyright © 2017年 MTI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
